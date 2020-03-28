9a12c73113ea011bae864fa25c6dc756dd27fb84
amindeist <amindeist@gmail.com>
2020-03-28 23:25:24 +0430

    Display All Questions

--- app/Question.php
+++ app/Question.php
@@ -4,6 +4,37 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Question
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $slug
+ * @property string $body
+ * @property int $views
+ * @property int $answers
+ * @property int $votes
+ * @property int|null $best_answer_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $user_id
+ * @property-read \App\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereAnswers($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereBestAnswerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereViews($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereVotes($value)
+ * @mixin \Eloquent
+ */
 class Question extends Model
 {
     protected $fillable = ['title','slug'];
